dab3e0c6ef87f48bbbc5997fdb96872333c0278b
51600100 <43369623+rongcon973@users.noreply.github.com>
2018-09-23 23:21:15 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class HelloWorld{
   public static void main(String[] args){
-    System.out.println("Hello World , I am TDTU ");
+    System.out.println("Hello World , This is TDTU ");
   }
 }
